1bd4c5343b5438f0b7c140643127f8b1db158c47
tanglw2020 <t17773460263@163.com>
2020-10-14 10:26:04 +0800

    feat: add word test

--- polls/admin.py
+++ polls/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
-from .models import WordOperations, WordQuestion
+from .models import WordOperations, WordQuestion, WordDocxFile, WordDocxFileTest
 # Register your models here.
 
 class WordOperationsAdmin(admin.ModelAdmin):
 
     list_display = ('para_text_simple', 'operations_list')
 
+    # 'collapse',
     fieldsets  = (
         ('段落内容', { 'fields': ('para_text','pub_date')}), 
-        ('文字编辑', { 'classes': ('collapse', 'extrapretty'), 'fields': ('char_edit_op','char_edit_origin','char_edit_replace')}), 
-        ('字体设置', { 'classes': ('collapse', 'extrapretty'), 'fields': ('font_op', 'font_name_chinese','font_name_ascii','font_size',
+        ('文字编辑', { 'classes': ( 'extrapretty'), 'fields': ('char_edit_op','char_edit_origin','char_edit_replace')}), 
+        ('字体设置', { 'classes': ( 'extrapretty'), 'fields': ('font_op', 'font_name_chinese','font_name_ascii','font_size',
                                  'font_underline', 'font_color', ('font_bold','font_italic'),
                                 )}), 
-        ('段落格式设置', { 'classes': ('collapse', 'extrapretty'), 'fields': ('paraformat_op','para_alignment', 'para_left_indent','para_right_indent',
+        ('段落格式设置', { 'classes': ( 'extrapretty'), 'fields': ('paraformat_op','para_alignment', 'para_left_indent','para_right_indent',
                                 'para_first_line_indent','para_first_line_indent_size',
                                 'para_firstchardropcap','para_firstchardropcaplines',
                                 'para_space_before','para_space_after',
@@ -23,6 +24,9 @@ class WordOperationsAdmin(admin.ModelAdmin):
 
 class WordQuestionAdmin(admin.ModelAdmin):
         filter_horizontal = ['word_operation_list']
+        list_display = ('file_path','word_op_numb','word_op_description', 'word_test_result')
 
 admin.site.register(WordOperations, WordOperationsAdmin)
 admin.site.register(WordQuestion, WordQuestionAdmin)
+admin.site.register(WordDocxFile)
+admin.site.register(WordDocxFileTest)
--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,24 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from docx import Document
 from .choices import *
+
+import os
 
 # Create your models here.
 class WordOperations(models.Model):
 
+    class Meta:
+        verbose_name = 'Word操作'
+        verbose_name_plural = 'Word操作'
+
     def para_text_simple(self):
         return self.para_text[:10] +'...'+self.para_text[-10:]
+    para_text_simple.short_description = '考查段落内容'
 
     def operations_list(self):
-            op_list = '考查：'
+            op_list = ''
             if self.char_edit_op:
                 op_list += '#文字查找替换  '
             if self.font_op:
@@ -16,7 +26,6 @@ class WordOperations(models.Model):
             if self.paraformat_op:
                 op_list += '#段落格式设置  '
             return op_list
-    para_text_simple.short_description = '考查内容'
     operations_list.short_description = '操作列表'
 
     pub_date = models.DateTimeField('创建时间')
@@ -57,9 +66,84 @@ class WordOperations(models.Model):
     keep_together = models.BooleanField('段中不分页', default=False, blank=True)
     window_control = models.BooleanField('孤行控制', default=False, blank=True)
 
+def validate_docx(value):
+    # file_path = value.path
+    # print(value.path, value.name, new_path)
+
+    extension = value.name.split('.')[-1]
+    if extension != 'docx':
+        raise ValidationError(
+            _(value.name+'不是docx文件'),
+            params={'value': value.name},
+        )
+
+    # try:
+    #     doc = Document(file_path)
+    # except:
+    #     raise ValidationError(
+    #         _(file_path+'不是有效的docx文件'),
+    #         params={'value': file_path},
+    #     )
+        # print(file_path+'不是有效的docx文件')
+
+class WordDocxFile(models.Model):
+
+    upload = models.FileField(upload_to='uploads_docx/', null=True, blank=True, 
+    validators=[validate_docx])
+    class Meta:
+        verbose_name = '考试用Word文件'
+        verbose_name_plural = '考试用Word文件'
+        def __str__(self):
+            return self.upload.name
+
+class WordDocxFileTest(models.Model):
+    
+    upload = models.FileField(upload_to='uploads_docx_test/', null=True, blank=True, 
+    validators=[validate_docx])
+
+    class Meta:
+        verbose_name = '内部测试用word文件'
+        verbose_name_plural = '内部测试用word文件'
+        def __str__(self):
+            return self.upload.name
 
 # Create your models here.
 class WordQuestion(models.Model):
 
+    class Meta:
+        verbose_name = 'Word题目'
+        verbose_name_plural = 'Word题目'
+
+    def file_path(self):
+        return self.upload_docx.upload.name
+    file_path.short_description = '文件'
+
+    def word_op_numb(self):
+            return len(self.word_operation_list.all())
+    word_op_numb.short_description = '题目数量'
+
+    def word_op_description(self):
+            return [ x.operations_list()+'||' for x in self.word_operation_list.all()]
+    word_op_description.short_description = '题目内容'
+
+    def word_test_result(self):
+            return self.upload_docx.upload.name + "::" + self.upload_docx_test.upload.name
+    word_test_result.short_description = '测试结果'
+
+
+    #########
     pub_date = models.DateTimeField('创建时间')
-    word_operation_list = models.ManyToManyField(WordOperations, blank=True)
+
+    upload_docx =  models.ForeignKey(
+        WordDocxFile,
+        on_delete=models.CASCADE, null=True,
+        verbose_name='考试用Word文件(.docx)'
+    )
+    word_operation_list = models.ManyToManyField(WordOperations, blank=True,
+    verbose_name='操作题目列表')
+
+    upload_docx_test = models.ForeignKey(
+        WordDocxFileTest,
+        on_delete=models.CASCADE, null=True, blank=True,
+        verbose_name='内部测试用word文件.docx'
+    )
